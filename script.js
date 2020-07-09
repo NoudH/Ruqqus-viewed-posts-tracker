eaa035fed9c6d0b3f5bd5e0002a033409347a3bc
NoudH <noudhabraken@gmail.com>
2020-07-09 11:19:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,25 +7,29 @@
 // @match        https://ruqqus.com/*
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js
 // @grant        none
+// @homepageURL  https://github.com/NoudH/Ruqqus-viewed-posts-tracker
+// @supportURL   https://github.com/NoudH/Ruqqus-viewed-posts-tracker/issues
 // ==/UserScript==
 
-let viewedPosts = localStorage.getItem('viewedPosts') ? JSON.parse(localStorage.getItem('viewedPosts')) : []
+/* jshint esversion: 6 */
+
+let viewedPosts = localStorage.getItem('viewedPosts') ? JSON.parse(localStorage.getItem('viewedPosts')) : [];
 let cardCount = $('.card').length;
 
 const addStyle = (style) => {
     style = style instanceof Array ? style.join('\n') : style;
     $('head').append($('<style type="text/css">' + style + '</style>'));
-}
+};
 
 addStyle(`
 .viewedPost-link {
     color: #805ad5!important;
 }
-`)
+`);
 
 const viewedPostsContainsUrl = (url) => {
 	return viewedPosts.filter(x => url.includes(x.url)).length > 0;
-}
+};
 
 const rememberLink = ($el, url) => {
 	if(!viewedPostsContainsUrl(url)){
@@ -33,30 +37,30 @@ const rememberLink = ($el, url) => {
 		localStorage.setItem('viewedPosts', JSON.stringify(viewedPosts));
 		$el.addClass('viewedPost-link');
 	}
-}
+};
 
 const setupRuqesButton = () => {
 	$('a.RuqES-by-enefi--button').click(function() {
 		console.log('test');
 		var $linkEl = $(this).closest('div.card-block.text-left').find('a.stretched-link');
 		rememberLink($linkEl, $linkEl.attr('href'));
-	})
-}
+	});
+};
 
 const setupLinkClicked = () => {
 	$('a.stretched-link').mousedown(function() {
 		var $linkEl = $(this);
 		var url = $linkEl.attr('href');
 		rememberLink($linkEl, url);
-	})
-}
+	});
+};
 
 const setupViewedPosts = () => {
 	while(viewedPosts.length > 999) {
 		viewedPosts.shift();
 	}
 	$('a.stretched-link').filter((_, el) => viewedPostsContainsUrl($(el).attr('href'))).addClass('viewedPost-link');
-}
+};
 
 //FIXME: preferably this should trigger when the next page is loaded, but I don't think I can hook into RuqES onNextPageSuccessResponse() function.
 const setupInfiniteScrollReload = () => {
@@ -68,14 +72,14 @@ const setupInfiniteScrollReload = () => {
 			setupViewedPosts();
 			setupRuqesButton();
 		}
-	})
-}
+	});
+};
 
 const work = () => {
     setupViewedPosts();
 	setupLinkClicked();
 	setupRuqesButton();
 	setupInfiniteScrollReload();
-}
+};
 
 $(work);
